d077467cd691f70e40d63209df7f8c5bc48640ff
Gene <romanelli.geno@gmail.com>
2018-09-14 23:41:22 +0000

    minor updates

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -10,3 +10,33 @@ Router.route('/', {
   controller: 'HomeController',
   where: 'client'
 });
+
+Router.route('/insert_issue', {
+  name: 'insertIssue',
+  controller: 'IssuesController',
+  action: 'insert',
+  where: 'client'
+});
+
+Router.route('/issues_list', {
+  name: 'issuesList',
+  controller: 'IssuesController',
+  action: 'list',
+  where: 'client'
+});
+
+Router.route('/issues/:_id', {
+  name: 'editIssue',
+  controller: 'IssuesController',
+  action: 'edit',
+  where: 'client'
+});
+
+Router.onBeforeAction(function() {
+  if(!Meteor.user()){
+    this.render('AccessDenied');
+  }else
+  {
+    this.next();
+  }
+}, {only: ['issuesList', 'insertIssue']});
--- app/server/publish.js
+++ app/server/publish.js
@@ -1,1 +1,3 @@
-
+Meteor.publish('issues', function(userID) {
+    return Issues.find({createdBy: userID});
+});
